ef1a6d881d93013966c2a189ec281717a31872fd
Andreas Lind <andreaslindpetersen@gmail.com>
2018-10-13 18:56:13 +0200

    No need to serialize the requests

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const mitm = require('mitm-papandreou')();
 const http = require('http');
 const https = require('https');
 const pick = require('lodash.pick');
-const createSerializedRequestHandler = require('./createSerializedRequestHandler');
 
 const metadataPropertyNames = [
   ...require('messy').HttpRequest.metadataPropertyNames,
@@ -112,128 +111,125 @@ mitm
       socket.bypass();
     }
   })
-  .on(
-    'request',
-    createSerializedRequestHandler(async (req, res) => {
-      const clientSocket = req.connection._mitm.client;
-      const clientSocketOptions = req.connection._mitm.opts;
-      const metadata = {
-        ...pick(
-          clientSocketOptions.agent && clientSocketOptions.agent.options,
-          metadataPropertyNames
-        ),
-        ...pick(clientSocketOptions, metadataPropertyNames)
+  .on('request', async (req, res) => {
+    const clientSocket = req.connection._mitm.client;
+    const clientSocketOptions = req.connection._mitm.opts;
+    const metadata = {
+      ...pick(
+        clientSocketOptions.agent && clientSocketOptions.agent.options,
+        metadataPropertyNames
+      ),
+      ...pick(clientSocketOptions, metadataPropertyNames)
+    };
+
+    const { body, error } = await consumeReadableStream(req);
+
+    if (error) {
+      throw error;
+    }
+
+    bypassNextConnect = true;
+    const matchHostHeader =
+      req.headers.host && req.headers.host.match(/^([^:]*)(?::(\d+))?/);
+
+    let host;
+    let port;
+
+    // https://github.com/moll/node-mitm/issues/14
+    if (matchHostHeader) {
+      if (matchHostHeader[1]) {
+        host = matchHostHeader[1];
+      }
+      if (matchHostHeader[2]) {
+        port = parseInt(matchHostHeader[2], 10);
+      }
+    }
+
+    let couldBePackageMetadataRequest = false;
+    if (host === 'registry.npmjs.org') {
+      delete req.headers['accept-encoding']; // Save the trouble of decoding gzip/brotli/deflate
+      delete req.headers['if-none-match']; // Avoid 304s so we don't get to patch up the response
+      delete req.headers.connection;
+      couldBePackageMetadataRequest = true;
+      if (
+        req.url === '/messy' &&
+        req.headers.accept ===
+          'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*'
+      ) {
+        console.log('PATCH!');
+        req.headers.accept = 'application/json';
+      }
+    }
+    try {
+      const upstreamRequestOptions = {
+        encrypted: req.socket.encrypted,
+        headers: req.headers,
+        method: req.method,
+        host,
+        // default the port to HTTP values if not set
+        port: port || (req.socket.encrypted ? 443 : 80),
+        path: req.url,
+        body,
+        metadata
       };
 
-      const { body, error } = await consumeReadableStream(req);
+      const responseResult = await performRequest(upstreamRequestOptions);
 
-      if (error) {
-        throw error;
-      }
-
-      bypassNextConnect = true;
-      const matchHostHeader =
-        req.headers.host && req.headers.host.match(/^([^:]*)(?::(\d+))?/);
-
-      let host;
-      let port;
-
-      // https://github.com/moll/node-mitm/issues/14
-      if (matchHostHeader) {
-        if (matchHostHeader[1]) {
-          host = matchHostHeader[1];
-        }
-        if (matchHostHeader[2]) {
-          port = parseInt(matchHostHeader[2], 10);
-        }
-      }
-
-      let couldBePackageMetadataRequest = false;
-      if (host === 'registry.npmjs.org') {
-        delete req.headers['accept-encoding']; // Save the trouble of decoding gzip/brotli/deflate
-        delete req.headers['if-none-match']; // Avoid 304s so we don't get to patch up the response
-        delete req.headers.connection;
-        couldBePackageMetadataRequest = true;
+      setImmediate(async () => {
         if (
-          req.url === '/messy' &&
-          req.headers.accept ===
-            'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*'
+          couldBePackageMetadataRequest &&
+          /json/.test(responseResult.headers['content-type'])
         ) {
-          console.log('PATCH!');
-          req.headers.accept = 'application/json';
-        }
-      }
-      try {
-        const upstreamRequestOptions = {
-          encrypted: req.socket.encrypted,
-          headers: req.headers,
-          method: req.method,
-          host,
-          // default the port to HTTP values if not set
-          port: port || (req.socket.encrypted ? 443 : 80),
-          path: req.url,
-          body,
-          metadata
-        };
-
-        const responseResult = await performRequest(upstreamRequestOptions);
-
-        setImmediate(async () => {
+          const obj = JSON.parse(responseResult.body.toString('utf-8'));
+          let timeByVersion = obj.time;
           if (
-            couldBePackageMetadataRequest &&
-            /json/.test(responseResult.headers['content-type'])
+            !timeByVersion &&
+            upstreamRequestOptions.headers.accept &&
+            upstreamRequestOptions.headers.accept.includes(
+              'application/vnd.npm.install-v1+json'
+            )
           ) {
-            const obj = JSON.parse(responseResult.body.toString('utf-8'));
-            let timeByVersion = obj.time;
-            if (
-              !timeByVersion &&
-              upstreamRequestOptions.headers.accept &&
-              upstreamRequestOptions.headers.accept.includes(
-                'application/vnd.npm.install-v1+json'
-              )
-            ) {
-              upstreamRequestOptions.headers = {
-                ...upstreamRequestOptions.headers,
-                accept: 'application/json'
-              };
-              bypassNextConnect = true;
+            upstreamRequestOptions.headers = {
+              ...upstreamRequestOptions.headers,
+              accept: 'application/json'
+            };
+            bypassNextConnect = true;
 
-              const fullMetadataResult = await performRequest(
-                upstreamRequestOptions
-              );
-              timeByVersion = JSON.parse(
-                fullMetadataResult.body.toString('utf-8')
-              ).time;
-            }
-
-            const changesMade = removeVersionsFromPackageMetadata(
-              obj,
-              timeByVersion
+            const fullMetadataResult = await performRequest(
+              upstreamRequestOptions
             );
+            timeByVersion = JSON.parse(
+              fullMetadataResult.body.toString('utf-8')
+            ).time;
+          }
 
-            if (changesMade) {
-              responseResult.body = Buffer.from(
-                JSON.stringify(obj, undefined, '  ')
-              );
-              responseResult.headers['content-length'] = String(
-                responseResult.body.length
-              );
-              delete responseResult.headers['transfer-encoding'];
-              delete responseResult.headers['content-encoding'];
-              responseResult.headers.connection = 'close';
-            }
+          const changesMade = removeVersionsFromPackageMetadata(
+            obj,
+            timeByVersion
+          );
+
+          if (changesMade) {
+            responseResult.body = Buffer.from(
+              JSON.stringify(obj, undefined, '  ')
+            );
+            responseResult.headers['content-length'] = String(
+              responseResult.body.length
+            );
+            delete responseResult.headers['transfer-encoding'];
+            delete responseResult.headers['content-encoding'];
+            responseResult.headers.connection = 'close';
           }
-          res.statusCode = responseResult.statusCode;
-          for (const headerName of Object.keys(responseResult.headers)) {
-            res.setHeader(headerName, responseResult.headers[headerName]);
-          }
-          res.end(responseResult.body);
-        });
-      } catch (err) {
-        clientSocket.emit('error', err);
-      }
-    })
-  );
+        }
+        res.statusCode = responseResult.statusCode;
+        for (const headerName of Object.keys(responseResult.headers)) {
+          res.setHeader(headerName, responseResult.headers[headerName]);
+        }
+        res.end(responseResult.body);
+      });
+    } catch (err) {
+      clientSocket.emit('error', err);
+    }
+  });
 
 // Run the wrapped executable:
 require('spawn-wrap').runMain();
